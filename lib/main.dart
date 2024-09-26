daacc05db6b28d4d19ac4f998ea759818006ed1a
Lorenzo Lasam <lorenzo.lasam@awsys-i.com>
2024-09-26 14:43:36 +0800

    Initial Commit for Beacon Library Test

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,49 @@
-
-// Importing important packages require to connect
-// Flutter and Dart
-import 'dart:async';
-import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
-import 'package:device_uuid/device_uuid.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:moyai_test_libraries/requirement_state_controller.dart';
+import 'package:moyai_test_libraries/home_page.dart';
 
-
-// Main Function
-
-void main() => runApp(MaterialApp(
-  home: Moyai(),
-));
-
-class Moyai extends StatefulWidget {
-  @override
-
-  _MoyaiState createState() => _MoyaiState();
+void main() {
+  runApp(Moyai());
 }
 
-class _MoyaiState extends State<Moyai> {
-  String _uuid = 'Unknown';
-  final _deviceUuidPlugin = DeviceUuid();
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String uuid;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      uuid = await _deviceUuidPlugin.getUUID() ?? 'Unknown uuid version';
-    } on PlatformException {
-      uuid = 'Failed to get uuid version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _uuid = uuid;
-    });
-  }
-
+class Moyai extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Text('Test App'),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Text(
-              'Device UUID',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
+    Get.put(RequirementStateController());
+
+    final themeData = Theme.of(context);
+    final primary = Colors.blue;
+
+    return GetMaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: primary,
+        appBarTheme: themeData.appBarTheme.copyWith(
+          elevation: 0.5,
+          color: Colors.white,
+          actionsIconTheme: themeData.primaryIconTheme.copyWith(
+            color: primary,
+          ),
+          iconTheme: themeData.primaryIconTheme.copyWith(
+            color: primary,
+          ), systemOverlayStyle: SystemUiOverlayStyle.dark, toolbarTextStyle: themeData.primaryTextTheme.copyWith(
+            headline6: themeData.textTheme.headline6?.copyWith(
+              color: primary,
             ),
-            const SizedBox(height: 10.0),
-            Text(
-              _uuid,
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                fontWeight: FontWeight.bold,
-                fontSize: 28.0,
-                letterSpacing: 2.0,
-              ),
+          ).bodyText2, titleTextStyle: themeData.primaryTextTheme.copyWith(
+            headline6: themeData.textTheme.headline6?.copyWith(
+              color: primary,
             ),
-            const SizedBox(height: 10.0),
-          ],
+          ).headline6,
         ),
       ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: primary,
+      ),
+      home: HomePage(),
     );
   }
 }
